63fb822a3d9ae40e42d0d2ad6b137829d77e2159
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2025-10-26 12:47:15 +0200

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector("button");
+const changeColor = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  changeColor.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = changeColor.textContent;
+});
