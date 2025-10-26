1bfc01322f146753ce0b4f12a31475c40153f767
Volodymyr Salashchenko <salashchenkovladymyr@gmail.com>
2025-10-26 10:45:13 +0200

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    const enterEmail = document.querySelector('input[type="email"]');
+    const enterPassw = document.querySelector('input[type="password"]');
+    if (enterEmail.value.trim() === "" || enterPassw.value.trim() === "") {
+        alert('All form fields must be filled in');
+    }
+    else {
+        const formObject = {
+            email: enterEmail.value.trim(),
+            password: enterPassw.value.trim()
+        };
+        console.log(formObject);
+        form.reset();
+    }
+})
